f3503c47febf09fad8bc777ea8bd2966e3dd1d3d
Megala71 <megalakrishnakumar92@gmail.com>
2025-02-12 13:10:22 +0530

    Delete AdminRepository.java

--- AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.PlacementManagementSystem.Admin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public abstract class AdminRepository implements JpaRepository<Admin, Long> {
-
-}
